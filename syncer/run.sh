12ecf6bb3903b22939b6968430642c64d8e3c485
sam <huapox@126.com>
2023-02-06 13:33:54 +0800

    sq: syncer同步到TLS私仓 --- #解压后README.md会替换(更新README2.md)
    README $ cat <<EOF |sudo tee -a /etc/profile
    
    ###04: fixed>> auth.yml仓库标识，不用registry.前缀
    
    fix: drop registry.
    
    fix: drop registry.
    
    registry: drop  #DST2_DOMAIN
    
    rename README2
    
    fix notes 8143
    
    fix tpl 8143
    
    ## test推私服(tls)
    
    revert :8143
    
    rename README
    
    test222
    
    try drop :8143
    
    f
    
    fix sed; authyml=/tmp/auth.yml; cat auth.yml > $authyml
    
    fix plain
    
    # certs:  ref1, ref2
    
    # DOCKER_REGISTRY_DST2_DOMAIN=deploy.xxx.com.ssl
    
    ss
    
    fix :
    
    dst2="$DOCKER_REGISTRY_DST2_DOMAIN:8143/infrasync/$dst"
    
    cat <<EOF |sudo tee -a
    
    DOCKER_REGISTRY_DST2_DOMAIN
    
    DOCKER_REGISTRY_DST2_DOMAIN
    
    test -s ./image-syncer || tar -zxf $file
    
    test -s "$file" ||
    
    bin
    
    dst2="172.25.21.60:8143/infrasync/$dst"
    
    registry.172.25.21.60:8143:
    
    up run.sh

--- syncer/run.sh
+++ syncer/run.sh
@@ -1,23 +1,68 @@
+
+file=image-syncer-v1.3.1-linux-amd64.tar.gz
+test -s "$file" || curl -O -fSL https://ghproxy.com/https://github.com/AliyunContainerService/image-syncer/releases/download/v1.3.1/$file
+test -s ./image-syncer || tar -zxf $file #解压后README.md会替换(更新README2.md)
+
+function errExit(){
+   echo "ERR: $1"
+   exit 1
+}
 function startSync(){
 source /etc/profile
-sed -i "s/username: .*#ali/username: ${DOCKER_REGISTRY_USER_infrastSubUser2} #ali/g" auth.yml
-sed -i "s/password: .*#ali/password: ${DOCKER_REGISTRY_PW_infrastSubUser2} #ali/g" auth.yml
-sed -i "s/username: .*#hub/username: ${DOCKER_REGISTRY_USER_dockerhub} #hub/g" auth.yml
-sed -i "s/password: .*#hub/password: ${DOCKER_REGISTRY_PW_dockerhub} #hub/g" auth.yml
-cat auth.yml |grep -v password
+# DOCKER_REGISTRY_DST2_DOMAIN=deploy.xxx.com.ssl
+match=$(cat /etc/hosts |grep $DOCKER_REGISTRY_DST2_DOMAIN)
+
+# hosts
+# test -z "$match" && echo "172.25.21.60 $DOCKER_REGISTRY_DST2_DOMAIN" >> /etc/hosts
+if [ -z "$match" ]; then
+   cat <<EOF |sudo tee -a /etc/hosts
+172.25.21.60 $DOCKER_REGISTRY_DST2_DOMAIN
+EOF
+fi
+cat /etc/hosts |grep "$DOCKER_REGISTRY_DST2_DOMAIN"
+
+# $authyml
+authyml=/tmp/auth.yml; cat auth.yml > $authyml
+sed -i "s/username: .*#ali/username: ${DOCKER_REGISTRY_USER_infrastSubUser2} #ali/g" $authyml
+sed -i "s/password: .*#ali/password: ${DOCKER_REGISTRY_PW_infrastSubUser2} #ali/g" $authyml
+sed -i "s/username: .*#hub/username: ${DOCKER_REGISTRY_USER_dockerhub} #hub/g" $authyml
+sed -i "s/password: .*#hub/password: ${DOCKER_REGISTRY_PW_dockerhub} #hub/g" $authyml
+# registry.deploy.xxx.com.ssl:8143: #DST2_DOMAIN
+sed -i "s/.*#DST2_DOMAIN/${DOCKER_REGISTRY_DST2_DOMAIN}:8143: #DST2_DOMAIN/g" $authyml
+sed -i "s/username: .*#dpRegistry/username: ${DOCKER_REGISTRY_USER_dpinner} #dpRegistry/g" $authyml
+sed -i "s/password: .*#dpRegistry/password: ${DOCKER_REGISTRY_PW_dpinner} #dpRegistry/g" $authyml
+cat $authyml |grep -v password
+
+# certs: 
+# ref1: syncer's Dockerfile
+# mkdir -p /etc/ssl/certs && update-ca-certificates --fresh
+
+# ref2: .psu/dpregistry.sh
+# headless @ armbian in /opt |14:11:18  
+# $ sudo bash set-certs.sh 
+# $ find /etc/docker/certs.d/
+# /etc/docker/certs.d/deploy.xxx.com.ssl:8143/deploy.xxx.com.ssl.crt
 
 # --proc 1 #多了hub取不到
-./image-syncer --proc 1 --auth ./auth.yml --images ./images.yml --arch=amd64 --arch=arm64 #--arch amd64,arm64
+./image-syncer --proc 1 --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 #--arch amd64,arm64
 }
 
+function genImgList(){
 :> images.yml
-cat src.txt |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
+cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
 
    src=$one
-   dst=$(echo $one |sed "s^/^-^g")
-   dst="registry.cn-shenzhen.aliyuncs.com/infrasync/$dst"
-   echo "$src: $dst" >> images.yml
-done
+   plain=$(echo $one |sed "s^/^-^g")
+   dst="registry.cn-shenzhen.aliyuncs.com/infrasync/$plain"
+   # echo "$src: $dst" >> images.yml
 
+   # dst2="172.25.21.60:8143/infrasync/$dst"
+   dst2="$DOCKER_REGISTRY_DST2_DOMAIN:8143/infrasync222/$plain"
+   echo "$dst: $dst2" >> images.yml
+done
 cat images.yml
+}
+
+# test "" == "$1" && errExit "please with src.txt"
+genImgList src.txt #$1
 startSync
--- syncer/use.md
+++
@@ -1,4 +0,0 @@
-
-```bash
-wget https://ghproxy.com/https://github.com/AliyunContainerService/image-syncer/releases/download/v1.3.1/image-syncer-v1.3.1-linux-amd64.tar.gz
-```
