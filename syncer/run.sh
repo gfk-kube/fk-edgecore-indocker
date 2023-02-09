2a478961340397c0be63a3665033059d4b2fdc78
sam <huapox@126.com>
2023-02-09 18:14:45 +0800

    sq: syncer sync with k3s/registry/syncer --- registry.cn-shenzhen.aliyuncs.com/infrastlabs/env-centos:alma8.7-sdk-v2
    infrastlabs/env-centos:alma8.7-sdk-v2
    
    # +replaceAcc
    
    sync: full cp
    
    sync cp new
    
    sync
    
    format
    
    mv _ex
    
    notes all early
    
    nginx:1.22.0-alpine
    
    re aliyun

--- syncer/run.sh
+++ syncer/run.sh
@@ -23,28 +23,34 @@ cat /etc/hosts |grep "$DOCKER_REGISTRY_DST2_DOMAIN"
 
 # $authyml
 authyml=/tmp/auth.yml; cat auth.yml > $authyml
+# +replaceAcc
 sed -i "s/username: .*#ali/username: ${DOCKER_REGISTRY_USER_infrastSubUser2} #ali/g" $authyml
 sed -i "s/password: .*#ali/password: ${DOCKER_REGISTRY_PW_infrastSubUser2} #ali/g" $authyml
 sed -i "s/username: .*#hub/username: ${DOCKER_REGISTRY_USER_dockerhub} #hub/g" $authyml
 sed -i "s/password: .*#hub/password: ${DOCKER_REGISTRY_PW_dockerhub} #hub/g" $authyml
-# registry.deploy.xxx.com.ssl:8143: #DST2_DOMAIN
-sed -i "s/.*#DST2_DOMAIN/${DOCKER_REGISTRY_DST2_DOMAIN}:8143: #DST2_DOMAIN/g" $authyml
+
+# registry.deploy.xxx.com.ssl:18443: #DST2_DOMAIN
+sed -i "s/.*#DST2_DOMAIN/${DOCKER_REGISTRY_DST2_DOMAIN}:18443: #DST2_DOMAIN/g" $authyml
 sed -i "s/username: .*#dpRegistry/username: ${DOCKER_REGISTRY_USER_dpinner} #dpRegistry/g" $authyml
 sed -i "s/password: .*#dpRegistry/password: ${DOCKER_REGISTRY_PW_dpinner} #dpRegistry/g" $authyml
+# 
+edgeRegistry_USER=admin; edgeRegistry_PW=admin123
+sed -i "s/username: .*#edgeRegistry/username: ${edgeRegistry_USER} #edgeRegistry/g" $authyml
+sed -i "s/password: .*#edgeRegistry/password: ${edgeRegistry_PW} #edgeRegistry/g" $authyml
 cat $authyml |grep -v password
 
 # certs: 
-# ref1: syncer's Dockerfile
-# mkdir -p /etc/ssl/certs && update-ca-certificates --fresh
+   # ref1: syncer's Dockerfile
+   # mkdir -p /etc/ssl/certs && update-ca-certificates --fresh
 
-# ref2: .psu/dpregistry.sh
-# headless @ armbian in /opt |14:11:18  
-# $ sudo bash set-certs.sh 
-# $ find /etc/docker/certs.d/
-# /etc/docker/certs.d/deploy.xxx.com.ssl:8143/deploy.xxx.com.ssl.crt
+   # ref2: .psu/dpregistry.sh
+   # headless @ armbian in /opt |14:11:18  
+   # $ sudo bash set-certs.sh 
+   # $ find /etc/docker/certs.d/
+   # /etc/docker/certs.d/deploy.xxx.com.ssl:18443/deploy.xxx.com.ssl.crt
 
 # --proc 1 #多了hub取不到
-./image-syncer --proc 1 --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 #--arch amd64,arm64
+./image-syncer $proc --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 #--arch amd64,arm64
 }
 
 function genImgList(){
@@ -52,17 +58,24 @@ function genImgList(){
 cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
 
    src=$one
-   plain=$(echo $one |sed "s^/^-^g")
+   plain=$(echo $one |sed "s^registry.cn-shenzhen.aliyuncs.com/^^g" |sed "s^/^-^g")
    dst="registry.cn-shenzhen.aliyuncs.com/infrasync/$plain"
-   # echo "$src: $dst" >> images.yml
-
-   # dst2="172.25.21.60:8143/infrasync/$dst"
-   dst2="$DOCKER_REGISTRY_DST2_DOMAIN:8143/infrasync222/$plain"
-   echo "$dst: $dst2" >> images.yml
+   if [ "true" != "$tlsPrivate" ]; then
+      proc="--proc 1" #多了hub取不到
+      echo "$src: $dst" >> images.yml
+   else
+      proc="--proc 5"
+      # dst2="172.25.21.60:18443/infrasync/$dst"
+      dst2="$DOCKER_REGISTRY_DST2_DOMAIN:18443/infrasync/$plain"
+      echo "$dst: $dst2" >> images.yml
+   fi
 done
 cat images.yml
 }
 
 # test "" == "$1" && errExit "please with src.txt"
-genImgList src.txt #$1
+tlsPrivate=true2
+test "true" == "" && src=src.txt || src=src0.txt
+DOCKER_REGISTRY_DST2_DOMAIN="server.k8s.local"
+genImgList $src #$1
 startSync
