cfa6366d07417a89250115e3d876fe16020eec8d
sam#gemmi-win10 <sam@china.gz>
2023-03-04 20:17:32 +0800

    sq: _trans.sh>> sort clear-kedge --- chmod 755 /rootfs/usr/local/sbin/runc /rootfs/usr/local/bin/k3s*
    up notes
    
    fix /rootfs
    
    tree -h /rootfs
    
    sort clear kedge

--- _trans.sh
+++ _trans.sh
@@ -20,31 +20,18 @@ tree -L 2
 
 ################
 mkdir -p /rootfs/usr/local/cfssl /rootfs/usr/local/kedge /rootfs/usr/local/bin/ /rootfs/usr/local/sbin/ /rootfs/opt/cni/bin/
-# ctd
+# cri: ctd,fuse-overlayfs,cni,runc
 \cp -a $arch/containerd-1.6.15-linux-$arch/bin/* /rootfs/usr/local/bin/
 \cp -a $arch/containerd-fuse-overlayfs-1.0.5-linux-$arch/containerd-fuse-overlayfs-grpc /rootfs/usr/local/bin/
 \cp -a $arch/cni-plugins-linux-$arch-v1.2.0/* /rootfs/opt/cni/bin/
 \cp -a $arch/runc.$arch /rootfs/usr/local/sbin/runc
-# ctd_tools
+# cri_tools: crictl,nerdctl
 \cp -a $arch/crictl-v1.26.0-linux-$arch/crictl /rootfs/usr/local/bin/
 \cp -a $arch/nerdctl-1.1.0-linux-$arch/nerdctl /rootfs/usr/local/bin/docker
 
-# k3s,kedge
-# test "amd64" == "$arch" && k3=$arch/k3s || k3=$arch/k3s-$arch ; \cp -a $k3 /rootfs/usr/local/bin/k3s
-# \cp -a $arch/kubeedge-v1.10.3-linux-$arch/kubeedge-v1.10.3-linux-$arch /rootfs/usr/local/kedge/
-# \cp -a $arch/kubeedge-v1.12.1-linux-$arch/kubeedge-v1.12.1-linux-$arch /rootfs/usr/local/kedge/
 
-# v1.22.17; down_k3s_v1.23.15/
-test "amd64" == "$arch" && k3=$arch/k3s || k3=$arch/k3s-$arch ; \cp -a $k3 /rootfs/usr/local/bin/k3s-v1.22.17
-test "amd64" == "$arch" && k3=down_k3s_v1.23.15/$arch/k3s || k3=down_k3s_v1.23.15/$arch/k3s-$arch ; \cp -a $k3 /rootfs/usr/local/bin/k3s-v1.23.15
-# kedge: x3版本
-\cp -a $arch/kubeedge-v1.10.3-linux-$arch/kubeedge-v1.10.3-linux-$arch /rootfs/usr/local/kedge/
-\cp -a $arch/kubeedge-v1.12.1-linux-$arch/kubeedge-v1.12.1-linux-$arch /rootfs/usr/local/kedge/
-\cp -a $arch/kubeedge-v1.13.0-linux-$arch/kubeedge-v1.13.0-linux-$arch /rootfs/usr/local/kedge/
-
-
-# 23.2.4: +cfssl,dcp,go:supervisord
-if [ "amd64" == "$arch" ]; then
+# 23.2.4: +cfssl,dcp,gosv
+if [ "amd64" == "$arch" ]; then #只有x64版本
   \cp -a down_cfssl_x64/$arch/cfssl_1.6.3_linux_amd64 /rootfs/usr/local/cfssl/
   \cp -a down_cfssl_x64/$arch/cfssljson_1.6.3_linux_amd64 /rootfs/usr/local/cfssl/
   \cp -a down_cfssl_x64/$arch/cfssl-certinfo_1.6.3_linux_amd64 /rootfs/usr/local/cfssl/
@@ -57,7 +44,28 @@ test "amd64" == "$arch" && file=$arch/supervisord_0.7.3_Linux_64-bit/supervisord
 \cp -a $file /rootfs/usr/local/bin/go-supervisord
 chmod +x /rootfs/usr/local/bin/*
 
+
+# k3s,kedge
+# v1.22.17; down_k3s_v1.23.15/
+test "amd64" == "$arch" && k3=$arch/k3s || k3=$arch/k3s-$arch ; \cp -a $k3 /rootfs/usr/local/bin/k3s-v1.22.17
+test "amd64" == "$arch" && k3=down_k3s_v1.23.15/$arch/k3s || k3=down_k3s_v1.23.15/$arch/k3s-$arch ; \cp -a $k3 /rootfs/usr/local/bin/k3s-v1.23.15
+# kedge: x3版本
+\cp -a $arch/kubeedge-v1.10.3-linux-$arch/kubeedge-v1.10.3-linux-$arch /rootfs/usr/local/kedge/
+\cp -a $arch/kubeedge-v1.11.2-linux-$arch/kubeedge-v1.11.2-linux-$arch /rootfs/usr/local/kedge/
+\cp -a $arch/kubeedge-v1.12.1-linux-$arch/kubeedge-v1.12.1-linux-$arch /rootfs/usr/local/kedge/
+\cp -a $arch/kubeedge-v1.13.0-linux-$arch/kubeedge-v1.13.0-linux-$arch /rootfs/usr/local/kedge/
+# clear kedge
+  # cloud/admission/admission
+  # cloud/controllermanager/controllermanager
+  # cloud/csidriver/csidriver
+  # cloud/iptablesmanager/iptablesmanager
+  # version
+du -sh /rootfs/usr/local/kedge/
+find /rootfs/usr/local/kedge/ -type f |grep -Ev "cloudcore|edgecore" |sort |xargs rm -rf
+du -sh /rootfs/usr/local/kedge/
+
+# alter,view
 rm -f /rootfs/usr/local/bin/containerd-stress /rootfs/usr/local/bin/containerd-shim-runc-v1
-chmod 755 /rootfs/usr/local/sbin/runc /rootfs/usr/local/bin/k3s 
-tree /rootfs
+chmod 755 /rootfs/usr/local/sbin/runc /rootfs/usr/local/bin/k3s*
+tree -h /rootfs
 ls -lh /rootfs/usr/local/bin/ /rootfs/usr/local/sbin/ /rootfs/opt/cni/bin/
