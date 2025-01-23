57ba2b10a0a3633d811cc084895e26c1539c2c99
sam <huapox@126.com>
2025-01-23 17:00:06 +0800

    syncer/run.sh src=src.txt; format
    syncer/run.sh format
    
    syncer/run.sh format genImgList
    
    up
    
    s
    
    run.sh genImgList_v2
    
    rum.sh SYNC_ARCH
    
    run.sh \cp -a /tmp/image-syncer-x64 ./syncer;
    
    run.sh +usage
    
    run.sh #: >---
    
    run.sh ./syncer $proc --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 --arch=arm

--- syncer/run.sh
+++ syncer/run.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
-cur=$(cd "$(dirname "$0")"; pwd)
+cur=$(dirname $(readlink -f "$0"))
 cd $cur
 
-# file=image-syncer-v1.3.1-linux-amd64.tar.gz
-# test -s "$file" || curl -O -fSL https://ghproxy.com/https://github.com/AliyunContainerService/image-syncer/releases/download/v1.3.1/$file
-file=image-syncer-x64.tar.gz #oci support
-test -s "$file" || curl -O -fSL https://gitee.com/infrastlabs/fk-image-syncer/releases/download/v23.4.25/$file
-test -s ./image-syncer || tar -zxf $file #解压后README.md会替换(更新README2.md)
-cat image-syncer-x64 > image-syncer; chmod +x image-syncer
+
+test -z "$SYNC_ALI"  && SYNC_ALI=false
+test -z "$SYNC_LIST" && SYNC_LIST="src.txt"
+test -z "$SYNC_ARCH" && SYNC_ARCH="amd64,arm64"
+# 
+test -z "$ALI_URL"   && ALI_URL="registry.cn-shenzhen.aliyuncs.com"
+test -z "$ALI_IMG"   && ALI_IMG="infrasync/v2025"
+test -z "$LOCAL_URL" && LOCAL_URL="server.k8s.local:18443"
+test -z "$LOCAL_IP"  && LOCAL_IP="172.25.21.60"
+# test -z "$SYNC_LIST" && errExit "please with src.txt"
 
 function errExit(){
-   echo "ERR: $1"
-   exit 1
+  echo "ERR: $1"
+  exit 1
 }
-function startSync(){
-source /etc/profile
-# DOCKER_REGISTRY_DST2_DOMAIN=deploy.xxx.com.ssl
-match=$(cat /etc/hosts |grep $DOCKER_REGISTRY_DST2_DOMAIN)
 
-# hosts
-# test -z "$match" && echo "172.25.21.60 $DOCKER_REGISTRY_DST2_DOMAIN" >> /etc/hosts
-if [ -z "$match" ]; then
-   cat <<EOF |sudo tee -a /etc/hosts
-172.25.21.60 $DOCKER_REGISTRY_DST2_DOMAIN
-EOF
-fi
-cat /etc/hosts |grep "$DOCKER_REGISTRY_DST2_DOMAIN"
+# auth: hub仓,ali仓,本地仓
+function authConf(){
+  # hosts
+  LOCAL_URL2=${LOCAL_URL%:*}
+  cat /etc/hosts |grep -V $LOCAL_URL2 /tmp/hosts;  echo "$LOCAL_IP $LOCAL_URL2" >> /tmp/hosts
+  cat /tmp/hosts |sudo tee /etc/hosts; cat /etc/hosts |grep "$LOCAL_URL2" #view
 
-# $authyml
-authyml=/tmp/auth.yml; cat auth.yml > $authyml
-# +replaceAcc
-sed -i "s/username: .*#ali/username: ${DOCKER_REGISTRY_USER_infrastSubUser2} #ali/g" $authyml
-sed -i "s/password: .*#ali/password: ${DOCKER_REGISTRY_PW_infrastSubUser2} #ali/g" $authyml
-sed -i "s/username: .*#hub/username: ${DOCKER_REGISTRY_USER_dockerhub} #hub/g" $authyml
-sed -i "s/password: .*#hub/password: ${DOCKER_REGISTRY_PW_dockerhub} #hub/g" $authyml
+  # $authyml
+  authyml=/tmp/auth.yml; cat auth.yml > $authyml
+  # +replaceAcc
+  # hub
+  sed -i "s/username: .*#hub/username: ${DOCKER_REGISTRY_USER_dockerhub} #hub/g" $authyml
+  sed -i "s/password: .*#hub/password: ${DOCKER_REGISTRY_PW_dockerhub} #hub/g" $authyml
+  # ali
+  sed -i "s/username: .*#ali/username: ${DOCKER_REGISTRY_USER_infrastSubUser2} #ali/g" $authyml
+  sed -i "s/password: .*#ali/password: ${DOCKER_REGISTRY_PW_infrastSubUser2} #ali/g" $authyml
+  # LOCAL_DOMAIN # repo1.registry.local:$port
+  sed -i "s/username: .*#local/username: ${DOCKER_REGISTRY_USER_dpinner} #local/g" $authyml
+  sed -i "s/password: .*#local/password: ${DOCKER_REGISTRY_PW_dpinner} #local/g" $authyml
+  sed -i "s/.*#LOCAL_DOMAIN/${LOCAL_URL}: #LOCAL_DOMAIN/g" $authyml
+  cat $authyml |grep -v password
 
-# registry.deploy.xxx.com.ssl:18443: #DST2_DOMAIN
-sed -i "s/.*#DST2_DOMAIN/${DOCKER_REGISTRY_DST2_DOMAIN}:18443: #DST2_DOMAIN/g" $authyml
-sed -i "s/username: .*#dpRegistry/username: ${DOCKER_REGISTRY_USER_dpinner} #dpRegistry/g" $authyml
-sed -i "s/password: .*#dpRegistry/password: ${DOCKER_REGISTRY_PW_dpinner} #dpRegistry/g" $authyml
-# 
-edgeRegistry_USER=admin; edgeRegistry_PW=admin123
-sed -i "s/username: .*#edgeRegistry/username: ${edgeRegistry_USER} #edgeRegistry/g" $authyml
-sed -i "s/password: .*#edgeRegistry/password: ${edgeRegistry_PW} #edgeRegistry/g" $authyml
-cat $authyml |grep -v password
-
-# certs: 
+  # certs: ||insecure: true @auth.yml
    # ref1: syncer's Dockerfile
    # mkdir -p /etc/ssl/certs && update-ca-certificates --fresh
 
@@ -53,46 +48,84 @@ cat $authyml |grep -v password
    # headless @ armbian in /opt |14:11:18  
    # $ sudo bash set-certs.sh 
    # $ find /etc/docker/certs.d/
-   # /etc/docker/certs.d/deploy.xxx.com.ssl:18443/deploy.xxx.com.ssl.crt
-
-# --proc 1 #多了hub取不到
-# ./image-syncer $proc --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 #--arch amd64,arm64
-./image-syncer $proc --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 --arch=arm
+   #   /etc/docker/certs.d/deploy.xxx.com.ssl:$port/deploy.xxx.com.ssl.crt
 }
 
 function genImgList(){
-:> images.yml
-cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
-
-   src=$one
-   # plain=$(echo $one |sed "s^registry.cn-shenzhen.aliyuncs.com/^^g" |sed "s^/^-^g")
-   # dst="registry.cn-shenzhen.aliyuncs.com/infrasync/$plain"
-   # 
-   # ghcr.io/octohelm/harbor/registry-photon:v2.6.2 ##ns多级目录
-   local repo=$(echo $src |cut -d'/' -f1); 
-   local img=${src##*/}; 
-   local ns=$(echo $src |sed "s^$repo/^^g" |sed "s^/$img^^g"); 
-   plain=$(echo $ns |sed "s^/^-^g")
-   dst="registry.cn-shenzhen.aliyuncs.com/infrasync/${plain}-$img"
-
-   if [ "true" == "$syncAliyun" ]; then
+  :> images.yml
+  cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
+    # ghcr.io/octohelm/harbor/registry-photon:v2.6.2 ##ns多级目录
+    local repo=$(echo $one |cut -d'/' -f1); 
+    local img=${one##*/}; 
+    local ns=$(echo $one |sed "s^$repo/^^g" |sed "s^/$img^^g"); 
+    plain=$(echo $ns |sed "s^/^-^g")
+    
+    local src=$one
+    local dst="$ALI_URL/infrasync/${plain}-$img"
+    local dst2="$LOCAL_URL/infrasync/${plain}-$img"
+    if [ "true" == "$SYNC_ALI" ]; then
       proc="--proc 1" #多了hub取不到
       echo "$src: $dst" >> images.yml
-   else
+    else
       proc="--proc 5"
-      # dst2="172.25.21.60:18443/infrasync/$dst"
-      dst2="$DOCKER_REGISTRY_DST2_DOMAIN:18443/infrasync/$plain"
       echo "$dst: $dst2" >> images.yml
-   fi
-done
-
-cat images.yml
+    fi
+  done
+  cat images.yml #view
+}
+# v2: 单个img:ns--img-tag
+function genImgList_v2(){
+  :> images.yml
+  cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
+    # ghcr.io/octohelm/harbor/registry-photon:v2.6.2 ##ns多级目录
+    local repo=$(echo $one |cut -d'/' -f1); 
+    local imgtag=$(echo $one |sed "s^$repo/^^g"); 
+    plain=$(echo $imgtag |sed "s^/^--^g") #/ >--
+    plain=$(echo $plain |sed "s^:^---^g") #: >---
+    
+    local src=$one
+    local dst="$ALI_URL/$ALI_IMG:${plain}"
+    local dst2="$LOCAL_URL/$ALI_IMG:${plain}"
+    if [ "true" == "$SYNC_ALI" ]; then
+      proc="--proc 1" #多了hub取不到
+      echo "$src: $dst" >> images.yml
+    else
+      proc="--proc 5"
+      echo "$dst: $dst2" >> images.yml
+    fi
+  done
+  cat images.yml #view
 }
 
-# test "" == "$1" && errExit "please with src.txt"
-syncAliyun=true
-# test "true" == "" && src=src.txt || src=src2.txt
-src=src2.txt
-DOCKER_REGISTRY_DST2_DOMAIN="server.k8s.local"
-genImgList $src #$1
-startSync #amd64,arm64
+source /etc/profile #DOCKER_REGISTRY_USER/PW_dockerhub
+authConf
+genImgList_v2 $SYNC_LIST
+
+# https://github.com/AliyunContainerService/image-syncer/releases/download/v1.3.1/image-syncer-v1.3.1-linux-amd64.tar.gz
+file=image-syncer-x64.tar.gz #oci support
+test -s "$file" || curl -s -k -o /tmp/$file -fSL https://gitee.com/infrastlabs/fk-image-syncer/releases/download/v23.4.25/$file
+tar -zxf /tmp/$file -C /tmp #解压后README.md会替换(更新README2.md)
+# gitac: cp: cannot create regular file '/bin/syncer': Permission denied
+\cp -a /tmp/image-syncer-x64 ./syncer; #chmod +x /bin/syncer
+
+# --proc 1 #多了hub取不到
+# --arch $SYNC_ARCH ##16.04; 14.04, mismatch of os or architecture ##view: 变成全arch, 该方式无效
+#   TODO: --arch $SYNC_ARCH >> 转换为 --arch=amd64 --arch=arm64 --arch=arm
+./syncer $proc --auth $authyml --images ./images.yml --arch=amd64 --arch=arm64 --arch=arm
+
+
+exit 0
+# usage
+# export SYNC_ALI=true
+# export SYNC_LIST="src.txt"
+# export SYNC_ARCH="amd64,arm64"
+# # 
+# export ALI_URL="registry.cn-shenzhen.aliyuncs.com"
+# export ALI_IMG="infrasync/v2025"
+# export LOCAL_URL="server.k8s.local:18443"
+# export LOCAL_IP="172.25.21.60"
+
+
+export LOCAL_URL="harbor.xxx.com"
+export LOCAL_IP="172.25.20.115"
+bash syncer/run.sh
