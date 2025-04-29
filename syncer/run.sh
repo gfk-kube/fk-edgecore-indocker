f7a4a2b672466ddcae4ec8e60c6eb1ec7df1b1c8
sam <huapox@126.com>
2025-04-29 16:27:47 +0800

    syncer/run.sh 支持LOCAL_FORMAT; -- # export SYNC_ALI=true #default:false
    syncer/run.sh # 25.4.19: +ns, imgtag
    
    sycer/run.sh ns=$(echo $ns |sed "s^/^--^g") #ns.plain 如ns多级目录,暂转--{harbor不支持; registry:可?}
    
    syncer/run.sh format
    
    syncer/run.sh val=$(eval echo $LOCAL_FORMAT)
    
    syncer/run.sh nsfull/nslast
    
    sycner/src.txt fix: tag=${imgtag##*:}; img=${imgtag%%:*}
    
    syncer/run.sh test "$LOCAL_IP" == "$LOCAL_URL2" || echo "$LOCAL_IP $LOCAL_URL2" >> /tmp/hosts #URL为IP模式时不写入
    
    syncer/run.sh fix |grep -v
    
    syncer/run.sh cat /etc/hosts |egrep -v "^$LOCAL_URL2 "
    
    syncer/run.sh fix cat /etc/hosts |egrep -v " $LOCAL_URL2$"
    
    syncer/run.sh fix cat /etc/hosts |egrep -v " $LOCAL_URL2$" > /tmp/hosts;

--- syncer/run.sh
+++ syncer/run.sh
@@ -11,6 +11,7 @@ test -z "$ALI_URL"   && ALI_URL="registry.cn-shenzhen.aliyuncs.com"
 test -z "$ALI_IMG"   && ALI_IMG="infrasync/v2025"
 test -z "$LOCAL_URL" && LOCAL_URL="server.k8s.local:18443"
 test -z "$LOCAL_IP"  && LOCAL_IP="172.25.21.60"
+test -z "$LOCAL_FORMAT"  && LOCAL_FORMAT="" #keep infrasync/v2025:ns--img---tag
 # test -z "$SYNC_LIST" && errExit "please with src.txt"
 
 function errExit(){
@@ -22,8 +23,8 @@ function errExit(){
 function authConf(){
   # hosts
   LOCAL_URL2=${LOCAL_URL%:*}
-  cat /etc/hosts |grep -V $LOCAL_URL2 /tmp/hosts;  echo "$LOCAL_IP $LOCAL_URL2" >> /tmp/hosts
-  cat /tmp/hosts |sudo tee /etc/hosts; cat /etc/hosts |grep "$LOCAL_URL2" #view
+  cat /etc/hosts |egrep -v " $LOCAL_URL2$" > /tmp/hosts;  test "$LOCAL_IP" == "$LOCAL_URL2" || echo "$LOCAL_IP $LOCAL_URL2" >> /tmp/hosts #URL为IP模式时不写入
+  cat /tmp/hosts |sudo tee /etc/hosts; #cat /etc/hosts |grep "$LOCAL_URL2" #view
 
   # $authyml
   authyml=/tmp/auth.yml; cat auth.yml > $authyml
@@ -51,18 +52,19 @@ function authConf(){
    #   /etc/docker/certs.d/deploy.xxx.com.ssl:$port/deploy.xxx.com.ssl.crt
 }
 
+# v1: infrasync/ns-img:tag
 function genImgList(){
   :> images.yml
   cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
     # ghcr.io/octohelm/harbor/registry-photon:v2.6.2 ##ns多级目录
     local repo=$(echo $one |cut -d'/' -f1); 
-    local img=${one##*/}; 
-    local ns=$(echo $one |sed "s^$repo/^^g" |sed "s^/$img^^g"); 
-    plain=$(echo $ns |sed "s^/^-^g")
+    local imgtag=${one##*/}; 
+    local ns=$(echo $one |sed "s^$repo/^^g" |sed "s^/$imgtag^^g"); 
+    nsplain=$(echo $ns |sed "s^/^-^g")
     
     local src=$one
-    local dst="$ALI_URL/infrasync/${plain}-$img"
-    local dst2="$LOCAL_URL/infrasync/${plain}-$img"
+    local dst="$ALI_URL/infrasync/${nsplain}-$imgtag"
+    local dst2="$LOCAL_URL/infrasync/${nsplain}-$imgtag"
     if [ "true" == "$SYNC_ALI" ]; then
       proc="--proc 1" #多了hub取不到
       echo "$src: $dst" >> images.yml
@@ -78,10 +80,19 @@ function genImgList_v2(){
   :> images.yml
   cat $1 |grep -Ev "^#|^$" |awk '{print $1}' |while read one; do
     # ghcr.io/octohelm/harbor/registry-photon:v2.6.2 ##ns多级目录
-    local repo=$(echo $one |cut -d'/' -f1); 
-    local imgtag=$(echo $one |sed "s^$repo/^^g"); 
-    plain=$(echo $imgtag |sed "s^/^--^g") #/ >--
+    local repo=$(echo $one |cut -d'/' -f1) 
+    local nsimgtag=$(echo $one |sed "s^$repo/^^g")
+    local plain=$(echo $nsimgtag |sed "s^/^--^g") #/ >--
     plain=$(echo $plain |sed "s^:^---^g") #: >---
+
+    # 25.4.19: +ns, imgtag
+    local imgtag=${nsimgtag##*/}; tag=${imgtag##*:}; img=${imgtag%%:*}
+    local ns=$(echo $nsimgtag |sed "s^/$imgtag^^g")
+    #ns.plain 如ns多级目录,  {harbor不支持; registry:可?}
+    #  1.nsfull: 暂转--
+    #  2.nslast: 只取最后一个:相对符合实用
+    nsfull=$(echo $ns |sed "s^/^--^g")
+    nslast=${ns##*/}
     
     local src=$one
     local dst="$ALI_URL/$ALI_IMG:${plain}"
@@ -90,6 +101,10 @@ function genImgList_v2(){
       proc="--proc 1" #多了hub取不到
       echo "$src: $dst" >> images.yml
     else
+      # 转私仓时允许自定格式;
+      local nsimgtag2=$(eval echo $LOCAL_FORMAT)
+      test -z "$nsimgtag2" || dst2="$LOCAL_URL/${nsimgtag2}" #非空时设定,如空则还是前面格式:infrasync/v2025:ns--img---tag
+
       proc="--proc 5"
       echo "$dst: $dst2" >> images.yml
     fi
@@ -116,7 +131,7 @@ tar -zxf /tmp/$file -C /tmp #解压后README.md会替换(更新README2.md)
 
 exit 0
 # usage
-# export SYNC_ALI=true
+# export SYNC_ALI=true #default:false
 # export SYNC_LIST="src.txt"
 # export SYNC_ARCH="amd64,arm64"
 # # 
@@ -128,4 +143,17 @@ exit 0
 
 export LOCAL_URL="harbor.xxx.com"
 export LOCAL_IP="172.25.20.115"
+export LOCAL_FORMAT='$nslast/$img:$tag' #nsfull/nslast; use '', not ""
 bash syncer/run.sh
+
+# eval https://blog.csdn.net/qq_35902025/article/details/140180780
+  # [root@(⎈|default:kubeedge) ~]$ ns=infrasync
+  # [root@(⎈|default:kubeedge) ~]$ img=alpine
+  # [root@(⎈|default:kubeedge) ~]$ tag=3.13.15
+  # [root@(⎈|default:kubeedge) ~]$ export LOCAL_FORMAT='$ns/$img:$tag'
+  # [root@(⎈|default:kubeedge) ~]$ eval $LOCAL_FORMAT
+  # bash: infrasync/alpine:3.13.15: No such file or directory
+  # 
+  # [root@(⎈|default:kubeedge) ~]$ val=$(eval echo $LOCAL_FORMAT)
+  # [root@(⎈|default:kubeedge) ~]$ echo $val
+  # infrasync/alpine:3.13.15
