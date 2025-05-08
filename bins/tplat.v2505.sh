36b59ef42fc77f463478297d14f84adaefd12767
sam <huapox@126.com>
2025-05-08 18:20:53 +0800

    tplat.v2505: dest="github.com/$URL" #默认github.com, cur: 全gh下载的

--- bins/tplat.v2505.sh
+++ bins/tplat.v2505.sh
@@ -23,8 +23,10 @@ old=$(pwd); mkdir -p $TARGETPLATFORM; cd $TARGETPLATFORM
 : > $cur/down/.list.txt
 function tplat(){
   TARGETPLATFORM=$1; URL=$2
-  dest=$(echo $URL |sed "s^https://ghproxy.com/^^g" |sed "s^hub.yzuu.cf^github.com^g" \
-    |sed "s^https://^^g" |sed "s^%2B^+^g") #%2B: +
+  # dest=$(echo $URL |sed "s^https://ghproxy.com/^^g" |sed "s^hub.yzuu.cf^github.com^g" \
+  #   |sed "s^https://^^g" |sed "s^%2B^+^g") #%2B: +
+  dest=$(echo $URL |sed "s^$GITHUB/^^g" |sed "s^https://^^g" |sed "s^%2B^+^g")
+  dest="github.com/$URL" #默认github.com, cur: 全gh下载的
   file=${dest##*/};dest=${dest%/*}; echo "destDir: $dest, file: $file"
 
   dst2=${cur}$WORKDIR/$TARGETPLATFORM/$dest; mkdir -p $dst2
